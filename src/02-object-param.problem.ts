9c60023d84fffe3ee5bffe764b757b16597e12f4
shawn-sandy <creative@shawnsandy.design>
2022-08-29 11:56:22 +0000

    fix 1 - 5

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
-  return params.first + params.second;
+export const addTwoNumbers = (params: { first: number, second: number}) => {
+  return params?.first + params?.second;
 };
 
 it("Should add the two numbers together", () => {
